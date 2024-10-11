77a01298a599f3fc474e7a139c64d124938871e6
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-11 09:35:43 +0200

    copy base app

---
+++ app/src/main/java/com/susanafigueroa/u4_dessert_clicker_viewmodel/model/Dessert.kt
@@ -0,0 +1,6 @@
+package com.susanafigueroa.u4_dessert_clicker_viewmodel.model
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
