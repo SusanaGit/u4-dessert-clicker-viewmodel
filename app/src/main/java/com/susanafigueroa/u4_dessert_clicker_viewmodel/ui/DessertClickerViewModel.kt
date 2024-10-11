c52b0cf82e13875cfa60ff5f3c74d7bc9a161ea7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-11 10:50:21 +0200

    create _uiState and uiState vals

--- app/src/main/java/com/susanafigueroa/u4_dessert_clicker_viewmodel/ui/DessertClickerViewModel.kt
+++ app/src/main/java/com/susanafigueroa/u4_dessert_clicker_viewmodel/ui/DessertClickerViewModel.kt
@@ -1,6 +1,16 @@
 package com.susanafigueroa.u4_dessert_clicker_viewmodel.ui
 
 import androidx.lifecycle.ViewModel
+import com.susanafigueroa.u4_dessert_clicker_viewmodel.data.DessertUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class DessertClickerViewModel : ViewModel() {
+
+    // Emits and saves the current state and notifies observers every time the state changes.
+    private val _uiState = MutableStateFlow(DessertUiState())
+
+    // Exposes a state that can only be modified within this class. Other components can observe it.
+    val uiState: StateFlow<DessertUiState> = _uiState.asStateFlow()
 }
